e01d6a00479ba9930f585c2af04940b3b40f9564
Frantz Gauthier <frantz@yutani.local>
2015-04-25 12:01:10 +0200

    Scaffolding imswann with yeoman/generator-gulp-webapp

---
+++ app/scripts/main.js
@@ -0,0 +1,45 @@
+/* jshint devel:true */
+(function($) {
+
+  var imgSrc = 'images/swann.jpg';
+
+  var loadCallback = function() {
+    setTimeout(function(){
+      if ('scroll' in window) {
+        window.scroll(0,0);
+      }
+      $('#bg').css('background-image', 'url('+imgSrc+')');
+      $(document.body).addClass('loaded');
+    }, 1500);
+
+  };
+
+  var img = new Image();
+  img.onload = loadCallback;
+  img.src = imgSrc;
+
+  window.jsonFlickrFeed = function(data) {
+    if (data && data.items && data.items.length) {
+      var $container = $("#galery p:first");
+
+      data.items.forEach(function(item) {
+        var date = new Date(item.date_taken);
+        var dateString = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+
+        if (item.media && item.media.m) {
+          $('<figure>').attr({
+            'class': 'thumbnail'
+          })
+            .append($("<div>").attr('style', 'background-image: url(' + item.media.m + ')'))
+            .append($("<figcaption>").text(dateString + ' - ' + item.title))
+            .appendTo($container)
+            .click(function() {
+              document.location = item.link;
+            });
+        }
+      });
+    }
+  };
+
+  $('<script defer src="http://api.flickr.com/services/feeds/photos_public.gne?id=48362780@N00&format=json">').appendTo(document.body);
+})(jQuery);
